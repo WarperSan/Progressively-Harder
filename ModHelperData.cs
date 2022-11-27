26cb6f84180a0845aaeca12f5bfb3df8dc478572
WarperSan <78861227+WarperSan@users.noreply.github.com>
2022-11-27 11:08:23 -0500

    Add files via upload

---
+++ ModHelperData.cs
@@ -0,0 +1,20 @@
+﻿using Assets.Scripts.Models.Towers;
+using BTD_Mod_Helper;
+using BTD_Mod_Helper.Api.Towers;
+using BTD_Mod_Helper.Extensions;
+using MelonLoader;
+
+namespace ProgressivelyHarder
+{
+    public static class ModHelperData
+    {
+        public const string Version = "1.1.0";
+        public const string Name = "Progressively Harder";
+        public const string Description =
+            "Make the game progressively harder by removing most used upgrades";
+
+        public const string RepoOwner = "WarperSan";
+        public const string RepoName = "BTD6-Mods";
+        public const string Icon = "up_arrow.png";
+    }
+}
